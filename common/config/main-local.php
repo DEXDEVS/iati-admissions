727276b355dd0847966e2ff43e6373e5f3f35262
anasshafqat <anasshafqat01@gmail.com>
2021-06-27 23:23:21 -0700

    Update DB

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,20 +1,20 @@
 <?php
 return [
     'components' => [
-        // 'db' => [
-        //     'class' => 'yii\db\Connection',
-        //     'dsn' => 'mysql:host=localhost;dbname=ranaifwh_wp670',
-        //     'username' => 'ranaifwh_wp670',
-        //     'password' => '7w1oS09-p]@)LL',
-        //     'charset' => 'utf8',
-        // ],
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=iati_db',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=ranaifwh_wp670',
+            'username' => 'ranaifwh_wp670',
+            'password' => '7w1oS09-p]@)LL',
             'charset' => 'utf8',
         ],
+        // 'db' => [
+        //     'class' => 'yii\db\Connection',
+        //     'dsn' => 'mysql:host=localhost;dbname=iati_db',
+        //     'username' => 'root',
+        //     'password' => '',
+        //     'charset' => 'utf8',
+        // ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
